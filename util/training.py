96841b5e6ff3663cbbde7426e634531c7540c263
Charlie Winter <charliewinter.vfx@gmail.com>
2021-02-03 13:03:19 -0800

    Updating docstrings

--- util/training.py
+++ util/training.py
@@ -1,6 +1,6 @@
+import math
 import numpy as np
 import torch
-import math
 
 
 ############################################################################
@@ -9,7 +9,17 @@ import math
 
 
 def set_lr_sched(epochs, iters, mult):
-    # lr_schedule, cyclical learning rate
+    """ Learning rate schedule, using cyclical learning rate
+
+    Args:
+      epochs (int): how many cycles to build
+      iters (int): iter count for first epoch
+      mult (float): how much longer each epoch will be
+
+    Returns:
+      list: float values representing learning rate during training
+    """
+
     mult_iter = iters
     iter_stack = []
     for a in range(epochs):
@@ -19,7 +29,13 @@ def set_lr_sched(epochs, iters, mult):
 
 
 def set_opt_lr(opt, lr):
-    # Set varried learn rate
+    """ Sets optimizer learning rate, using different values for each sub group
+
+    Args:
+      opt (torch.optimizer): optimizer to set learning rate on
+      lr (np.array): array of learning rate values
+    """
+
     opt.param_groups[0]['lr'] = lr[0]
     opt.param_groups[1]['lr'] = lr[1]
     opt.param_groups[2]['lr'] = lr[2]
@@ -31,7 +47,21 @@ def set_opt_lr(opt, lr):
 
 
 def train(train_dict, x, y, lr, lr_lookup, current_iter):
-    # Train one iter
+    """ Train network for one iter
+
+    Args:
+      train_dict (dict): dictionary containing network, optimizer, loss and loader
+      x (torch.tensor): input image from dataset
+      y (torch.tensor): actual y value from dataset
+      lr (float): base learning rate overall multiplier
+      lr_lookup (list): float values representing learning rate during training
+      current_iter (int): which iteration we are on, used to lookup learning rate
+
+    Returns:
+      torch.tensor: mse loss
+      torch.tensor: point loss
+    """
+
     lr_mult = lr_lookup[current_iter]
     set_opt_lr(train_dict["opt"], lr * lr_mult)
 
@@ -39,6 +69,8 @@ def train(train_dict, x, y, lr, lr_lookup, current_iter):
     yhat = train_dict["net"](x)
     mse_loss = train_dict["mse_crit"](yhat, y.float())
     point_loss = train_dict["point_crit"](yhat, y.float())
+
+    # Testing with no point loss, works better in some situations
     loss = mse_loss  # +  point_loss  #  might not help
     loss.backward()
     train_dict["opt"].step()
@@ -46,7 +78,18 @@ def train(train_dict, x, y, lr, lr_lookup, current_iter):
 
 
 def test(train_dict, x, y):
-    # Test one iter
+    """ Test network for one iter
+
+    Args:
+      train_dict (dict): dictionary containing network, optimizer, loss and loader
+      x (torch.tensor): input image from dataset
+      y (torch.tensor): actual y value from dataset
+
+    Returns:
+      torch.tensor: mse loss
+      torch.tensor: point loss
+    """
+
     yhat = train_dict["net"](x)
     mse_loss = train_dict["mse_crit"](yhat, y.float())
     point_loss = train_dict["point_crit"](yhat, y.float())
@@ -54,11 +97,20 @@ def test(train_dict, x, y):
 
 
 def one_run(train_dict, freeze, lr_list, lr_array):
-    # Run through one set of cyclical leanring
+    """ Run through one cycle of cyclical learning
+
+    Args:
+      train_dict (dict): dictionary containing network, optimizer, loss and loader
+      freeze (int): how many layers to freeze on the model
+      lr_list (float): float values representing learning rate during training
+      lr_array (np.array): array of learning rate values
+    """
+
     current_iter = 0
     current_epoch = 0
     done = False
     train_dict["net"].set_freeze(freeze)
+
     while not done:
         epoch_test_mse_loss = []
         epoch_train_mse_loss = []
